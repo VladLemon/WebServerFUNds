4cf5d12912584e7c842e458982918191aded86b5
VladLemon <khilimonchyk.vlad@gmail.com>
2023-10-04 13:15:44 -0400

    Update page.php

--- page.php
+++ page.php
@@ -7,7 +7,7 @@
     <br><br><br>
 
     <h1>Little Forms:</h1>
-    <form action="form.php" method="get">
+    <form action="form.php" method="post">
         <p>Your name: <input type="text" name="fullname"/></p>
         <p>Your age: <input type="text" name="age"/></p>
         <p>Your Email: <input type="text" name="email"/></p>
