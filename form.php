d10e8a941ca08d17afa2f41a17a0ef11c6f35093
VladLemon <khilimonchyk.vlad@gmail.com>
2023-10-04 13:25:25 -0400

    Update form.php

--- form.php
+++ form.php
@@ -2,9 +2,11 @@
 <html>
 <body>
 
-<p><?= var_dump($_GET) ?></p>
-<br>
-<p><?= var_dump($_POST) ?></p>
+<h1>Form Responce</h1>
+
+<p> Your name is: <?= htmlspecialchars($_POST['fullname'])?></p>
+<p> Your age is: <?= htmlspecialchars($_POST['age'])?></p>
+<p> Your email is: <?= htmlspecialchars($_POST['email'])?></p>
 
 </body>
 </html>
