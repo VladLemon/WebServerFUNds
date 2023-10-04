b418d1316d0a3d7863447c04c04c8c1a8e91937d
VladLemon <khilimonchyk.vlad@gmail.com>
2023-10-04 13:08:59 -0400

    Update page.php

--- page.php
+++ page.php
@@ -5,13 +5,17 @@
     <h1>This is your user agent:</h1>
     <p><?= $_SERVER['HTTP_USER_AGENT']?></p> 
     <br><br><br>
-    <h1>Little Forms:</h1>
-    <label for="fname">First name:</label><br />
-    <input type="text" id="fname" name="fname" /><br />
-    <label for="lname">Last name:</label><br />
-    <input type="text" id="lname" name="lname" /><br />
-    <label for="age">Age:</label><br />
-    <input type="text" pattern="\d" id="age" name="age" /><br />
 
+    <h1>Little Forms:</h1>
+    <form action="form.php" method="get">
+        <p>Your name: <input type="text" name="fullname"/></p>
+        <p>Your age: <input type="text" name="age"/></p>
+        <p>Your Email: <input type="text" name="email"/></p>
+        <p><input type="submit"/></p>
+    </form>
+
+    <p><?= var_dump($_GET) ?></p>
+    <p><?= var_dump($_POST) ?></p>
+    
 </body>
 </html>
