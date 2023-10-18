e8ed08d522229251360e4b0d1afa3de6e446aa69
VladLemon <143566522+VladLemon@users.noreply.github.com>
2023-10-18 14:40:17 -0400

    Update page.php

--- page.php
+++ page.php
@@ -23,27 +23,36 @@
 </body>
 
 <body>
+    <h3>Data base: </h3>
+    This are the anime in the database table
+    <?php
+        foreach($result as $row) 
+        {
+            echo "Top: {$row['anime_top_number']} | Name: {$row['anime_name']} | Episodes: {$row['total_num_episodes']}\n";
+        }
+        mysqli_close($conn);
+    ?>
 
     <form action="sqlans.php" method="get">
-            <label for="anime">Select anime</label><br/>
-            <select id="anime" name="anime">
-                <?php
-                    foreach($result as $row) 
-                    {
-                        echo "<option value='{$row['anime_top_number']}'> Top: {$row['anime_top_number']} | Name: {$row['anime_name']}</option>\n";
-                    }
-                    mysqli_close($conn);
-                ?>
-            </select>
-            <br/>
-            <input type="submit" value="submit"/>  
-        </form>
+        <label for="anime">Select anime</label><br/>
+        <select id="anime" name="anime">
+            <?php
+                foreach($result as $row) 
+                {
+                    echo "<option value='{$row['anime_top_number']}'> Top: {$row['anime_top_number']} | Name: {$row['anime_name']}</option>\n";
+                }
+                mysqli_close($conn);
+            ?>
+        </select>
+        <br/>
+        <input type="submit" value="submit"/>  
+    </form>
 
-    <h1>This is your user agent:</h1>
+    <h3>This is your user agent:</h3>
     <p><?= $_SERVER['HTTP_USER_AGENT']?></p> 
     <br><br><br>
 
-    <h1>Little Forms:</h1>
+    <h3>Little Forms:</h3>
     <form action="form.php" method="post">
         <p>Your name: <input type="text" name="fullname"/></p>
         <p>Your age: <input type="text" name="age"/></p>
