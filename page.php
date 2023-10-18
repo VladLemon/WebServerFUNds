d95ed6881bf581288af9a53ffc97a66b23a83706
VladLemon <143566522+VladLemon@users.noreply.github.com>
2023-10-18 14:04:04 -0400

    Lab7-1st try show sql
    Show info on page

--- page.php
+++ page.php
@@ -1,6 +1,43 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>SQL test</title>
+        <?php
+            $server = "localhost";
+            $username = "php";
+            $password = "password";
+            $database = "animelist";
+            $conn = mysqli_connect($server, $username, $password, $database);
+            
+            // Check for successful connection
+            if (!$conn) {
+              die("Connection failed: {mysqli_connect_error()}");
+            }
+            $sql = "select * from top;";
+            $result = mysqli_query($conn, $sql);
+        ?>
+    </head>
+
 <body>
+
+</body>
+
+<body>
+
+    <form action="sqlans.php" method="get">
+            <label for="anime">Select anime</label><br/>
+            <select id="anime" name="anime">
+                <?php
+                    foreach($result as $row) 
+                    {
+                        echo "<option value='{$row['anime_top_number']}'> Top: {$row['anime_top_number']} | Name: {$row['anime_name']}</option>\n";
+                    }
+                    mysqli_close($conn);
+                ?>
+            </select>
+            <br/>
+            <input type="submit" value="submit"/>  
+        </form>
 
     <h1>This is your user agent:</h1>
     <p><?= $_SERVER['HTTP_USER_AGENT']?></p> 
