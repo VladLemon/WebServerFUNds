de8d147600f3753ddfc60187620721ea676ddb4e
VladLemon <143566522+VladLemon@users.noreply.github.com>
2023-11-15 14:39:19 -0500

    Update UpdData.php

--- UpdData.php
+++ UpdData.php
@@ -1,13 +1,13 @@
 <?php
-$raw = `./bme280`; 
+    $raw = `./bme280`; 
     $deserialized = json_decode($raw, true);  
     var_dump($deserialized); 
-    echo "Temperature =";
+    echo "Temperature =, $";
     echo $deserialized["temperature"]; 
     echo "<br>";
     echo "Pressure =";
     echo $deserialized["pressure"];
     echo "<br>";
-    echo "Altitude ="
+    echo "Altitude =";
     echo $deserialized["altitude"];
 ?>
