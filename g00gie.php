93fc5c2a1189177c4de5f8803570afc377ad321b
VladLemon <143566522+VladLemon@users.noreply.github.com>
2024-01-12 12:54:56 -0500

    Update g00gie.php

--- g00gie.php
+++ g00gie.php
@@ -2,20 +2,6 @@
 <html>
     <head>
         <title>G00gie.ca</title>
-        <?php
-            $server = "localhost";
-            $username = "php";
-            $password = "password";
-            $database = "animelist";
-            $conn = mysqli_connect($server, $username, $password, $database);
-            
-            // Check for successful connection
-            if (!$conn) {
-              die("Connection failed: {mysqli_connect_error()}");
-            }
-            $sql = "select * from searches;";
-            $result = mysqli_query($conn, $sql);
-        ?>
     </head>
 
 <body>
