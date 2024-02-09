ed6f084c8f7390f07acfcd82355da4f4129884e6
VladLemon <khilimonchyk.vlad@gmail.com>
2024-02-09 12:47:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
             justify-content: center;
             align-items: center;
             min-height: 100vh;
-            background-image: url('https://protecsecuritysystems.com/wp-content/uploads/cache/2019/04/home-security-systems-icons-greenville-sc-/3896337847.jpg');
+            background-image: url('https://static.vecteezy.com/system/resources/previews/014/703/703/non_2x/smart-home-system-concept-phone-controls-works-safety-of-smart-devices-in-house-smartphone-app-of-monitoring-setting-of-algorithm-autonomous-works-of-household-appliances-internet-of-things-iot-vector.jpg');
             background-size: cover;
             background-position: center;
         }
