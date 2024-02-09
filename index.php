3a92e211e07e2da0cbdaf5020eab647f84eb3444
VladLemon <khilimonchyk.vlad@gmail.com>
2024-02-09 10:52:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -54,6 +53,8 @@
             opacity: 1;
         }
     </style>
+    <link rel="stylesheet" href="/css/pure/pure-min.css">
+    <link rel="stylesheet" href="/layouts/side-menu/styles.css">
 </head>
 
 <body>
@@ -64,27 +65,7 @@
         <a href="#">Contact <span></span></a>
         <a href="#">Help <span></span></a>
     </nav>
-</body>
-
-</html>
-
-<!-- <!doctype html>
-    <html lang="en">
-
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description"
-            content="A layout example with a side menu that hides on mobile, just like the Pure website.">
-        <title>Responsive Side Menu &ndash;
-            Layout Examples &ndash;
-            Pure</title>
-        <link rel="stylesheet" href="/css/pure/pure-min.css">
-        <link rel="stylesheet" href="/layouts/side-menu/styles.css">
-    </head>
-
-    <body>
-        <div id="layout">
+    <div id="layout">
             < !-- Menu toggle <a href="#menu" id="menuLink" class="menu-link">
                 < !-- Hamburger icon <span></span></a>
                     <div id="menu">
@@ -147,6 +128,8 @@
                     </div>
         </div>
         <script src="/js/ui.js"></script>
-    </body>
+</body>
 
-    </html>-->
+</html>
+
+    
